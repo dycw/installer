4bc83e967b00527800add1cb257540f67f0ae0ae
Derek Wan <d.wan@icloud.com>
2025-10-13 07:20:53 +0900

    '2025-10-13 07:20:53 (Mon)  > DW-Mac  > derekwan'

--- src/installer/constants.py
+++ src/installer/constants.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 from pathlib import Path
 
 HOME = Path("~").expanduser()
-XDG_CONFIG_HOME = HOME / ".config"
 LOCAL_BIN = HOME / ".local/bin"
 SSH = HOME / ".ssh"
+XDG_CONFIG_HOME = HOME / ".config"
 
 
 __all__ = ["HOME", "LOCAL_BIN", "SSH", "XDG_CONFIG_HOME"]
