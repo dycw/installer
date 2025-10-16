1e6c76282a103c9d0d4fb909298981684b5ce3b3
Derek Wan <d.wan@icloud.com>
2025-10-16 09:27:30 +0900

    '2025-10-16 09:27:30 (Thu)  > DW-Mac  > derekwan'

--- src/installer/utilities.py
+++ src/installer/utilities.py
@@ -206,7 +206,7 @@ def is_root() -> bool:
 
 
 def log_installer_version() -> None:
-    _LOGGER.info("'installer' version: 0.2.48")
+    _LOGGER.info("'installer' version: 0.2.49")
 
 
 def luarocks_install(package: str, /) -> None:
