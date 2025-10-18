b6624a070de6f81b28d6517dd7f07731fcb1a5a4
Derek Wan <d.wan@icloud.com>
2025-10-18 23:52:08 +0900

    Minor refactoring (#55)

--- src/installer/utilities.py
+++ src/installer/utilities.py
@@ -206,7 +206,7 @@ def is_root() -> bool:
 
 
 def log_installer_version() -> None:
-    _LOGGER.info("'installer' version: 0.2.52")
+    _LOGGER.info("'installer' version: 0.2.53")
 
 
 def luarocks_install(package: str, /) -> None:
