58625668c65c77bb1b0428e029294586a3a8733d
Derek Wan <d.wan@icloud.com>
2026-01-05 22:53:42 +0900

    2026-01-05 22:53:42 (Mon)  > DW-Mac  > derekwan

--- src/github_downloader/lib.py
+++ src/github_downloader/lib.py
@@ -7,9 +7,12 @@ from github import Github
 from github.Auth import Token
 from requests import get
 from typed_settings import Secret
-from utilities.iterables import OneEmptyError, OneNonUniqueError, one
+from utilities.inflect import counted_noun
+from utilities.iterables import OneNonUniqueError, one
 from utilities.subprocess import chmod
+from utilities.text import strip_and_dedent
 
+from github_downloader import __version__
 from github_downloader.constants import MACHINE_TYPE, SYSTEM_NAME
 from github_downloader.logging import LOGGER
 from github_downloader.settings import AGE_SETTINGS, SETTINGS, SOPS_SETTINGS
@@ -36,23 +39,68 @@ def setup_asset(
     permissions: str = SETTINGS.permissions,
 ) -> None:
     """Download a GitHub release."""
-    gh = Github(auth=None if token is None else Token(token.get_secret_value()))
-    repository = gh.get_repo(f"{owner}/{repo}")
-    release = repository.get_latest_release()
+    LOGGER.info(
+        strip_and_dedent("""
+            Running '%s' (version %s) with settings:
+             - owner         = %s
+             - repo          = %s
+             - binary_name   = %s
+             - token         = %s
+             - match_system  = %s
+             - match_machine = %s
+             - not_endswith  = %s
+             - timeout       = %s
+             - path_binaries = %s
+             - chunk_size    = %s
+             - permissions   = %s
+        """),
+        setup_asset.__name__,
+        __version__,
+        owner,
+        repo,
+        binary_name,
+        token,
+        match_system,
+        match_machine,
+        not_endswith,
+        timeout,
+        path_binaries,
+        chunk_size,
+        permissions,
+    )
     if SYSTEM_NAME not in {"Darwin", "Linux"}:
         msg = f"Invalid system {SYSTEM_NAME!r}"
         raise ValueError(msg)
+    gh = Github(auth=None if token is None else Token(token.get_secret_value()))
+    repository = gh.get_repo(f"{owner}/{repo}")
+    release = repository.get_latest_release()
     assets = list(release.get_assets())
+    LOGGER.info("Got %s: %s", counted_noun(assets, "asset"), [a.name for a in assets])
     if match_system:
         assets = [a for a in assets if search(SYSTEM_NAME, a.name, flags=IGNORECASE)]
+        LOGGER.info(
+            "Post system name, got %s: %s",
+            counted_noun(assets, "asset"),
+            [a.name for a in assets],
+        )
     if match_machine:
         assets = [a for a in assets if search(MACHINE_TYPE, a.name, flags=IGNORECASE)]
-    assets = [a for a in assets if all(not a.name.endswith(e) for e in not_endswith)]
+        LOGGER.info(
+            "Post machine type %r, got %s: %s",
+            counted_noun(assets, "asset"),
+            [a.name for a in assets],
+        )
+    if len(not_endswith) >= 1:
+        assets = [
+            a for a in assets if all(not a.name.endswith(e) for e in not_endswith)
+        ]
+        LOGGER.info(
+            "Post asset name endings, got %s: %s",
+            counted_noun(assets, "asset"),
+            [a.name for a in assets],
+        )
     try:
         asset = one(assets)
-    except OneEmptyError:
-        msg = f"No assets found amongst {[a.name for a in release.get_assets()]}"
-        raise ValueError(msg) from None
     except OneNonUniqueError as error:
         raise OneNonUniqueError(
             iterables=([a.name for a in assets],),
@@ -84,6 +132,25 @@ def setup_age(
     permissions: str = AGE_SETTINGS.permissions,
 ) -> None:
     """Download 'age'."""
+    LOGGER.info(
+        strip_and_dedent("""
+            Running '%s' (version %s) with settings:
+             - binary_name   = %s
+             - token         = %s
+             - timeout       = %s
+             - path_binaries = %s
+             - chunk_size    = %s
+             - permissions   = %s
+        """),
+        setup_age.__name__,
+        __version__,
+        binary_name,
+        token,
+        timeout,
+        path_binaries,
+        chunk_size,
+        permissions,
+    )
     setup_asset(
         "FiloSottile",
         "age",
@@ -109,6 +176,25 @@ def setup_sops(
     permissions: str = SOPS_SETTINGS.permissions,
 ) -> None:
     """Download 'sops'."""
+    LOGGER.info(
+        strip_and_dedent("""
+            Running '%s' (version %s) with settings:
+             - binary_name   = %s
+             - token         = %s
+             - timeout       = %s
+             - path_binaries = %s
+             - chunk_size    = %s
+             - permissions   = %s
+        """),
+        setup_sops.__name__,
+        __version__,
+        binary_name,
+        token,
+        timeout,
+        path_binaries,
+        chunk_size,
+        permissions,
+    )
     setup_asset(
         "getsops",
         "sops",
