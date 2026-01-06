e238827d1c8e844d7d460b2e1b280246c4fdbd64
Derek Wan <d.wan@icloud.com>
2026-01-06 10:36:35 +0900

    2026-01-06 10:36:35 (Tue)  > DW-Mac  > derekwan

---
+++ src/tests/test_cli.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from pytest import mark, param
+from utilities.subprocess import run
+
+
+class TestCLI:
+    @mark.parametrize(
+        "args",
+        [
+            param(["age"]),
+            param(["run", "asset-owner", "asset-repo", "binary-name"]),
+            param(["ripgrep"]),
+            param(["sops"]),
+        ],
+    )
+    def test_main(self, *, args: list[str]) -> None:
+        run("github-download", *args)
