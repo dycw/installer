13a043a59d07d270ec96b938a5fce4efee8ce42b
Derek Wan <d.wan@icloud.com>
2026-01-11 02:28:48 +0900

    Add shell setup for `direnv`, `fzf` and `starship` (#24)

--- src/github_downloader/utilities.py
+++ src/github_downloader/utilities.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
 
-from typing import assert_never
+from pathlib import Path
+from typing import TYPE_CHECKING, assert_never
 
 from typed_settings import Secret
+from utilities.atomicwrites import writer
+
+from github_downloader.constants import SHELL
+from github_downloader.logging import LOGGER
+
+if TYPE_CHECKING:
+    from utilities.types import PathLike
 
 
 def convert_token(x: str | None, /) -> Secret[str] | None:
@@ -25,4 +33,35 @@ def convert_token(x: str | None, /) -> Secret[str] | None:
             assert_never(never)
 
 
-__all__ = ["convert_token"]
+##
+
+
+def ensure_line(text: str, path: PathLike, /) -> None:
+    path = Path(path)
+    try:
+        contents = path.read_text()
+    except FileNotFoundError:
+        with writer(path) as temp:
+            _ = temp.write_text(text)
+        LOGGER.info("Wrote %r to %r", text, str(path))
+        return
+    if text not in contents:
+        with path.open(mode="a") as fh:
+            _ = fh.write(f"\n\n{text}")
+        LOGGER.info("Appended %r to %r", text, str(path))
+
+
+##
+
+
+def ensure_shell_rc(text: str, /) -> None:
+    match SHELL:
+        case "bash" | "zsh":
+            ensure_line(text, Path.home() / f".{SHELL}rc")
+        case "fish":
+            ensure_line(text, Path.home() / ".config/fish/config.fish")
+        case never:
+            assert_never(never)
+
+
+__all__ = ["convert_token", "ensure_line", "ensure_shell_rc"]
