ed0684cf7ac804fe9d4421791e27eb8fb8889605
Derek Wan <d.wan@icloud.com>
2026-01-15 09:22:48 +0900

    2026-01-15 09:22:48 (Thu)  > DW-Mac  > derekwan

--- src/tests/apps/test_lib.py
+++ src/tests/apps/test_lib.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from re import escape, search
 from typing import TYPE_CHECKING
 
-from pytest import mark
 from utilities.pytest import throttle_test
 from utilities.subprocess import run
 from utilities.text import strip_and_dedent
@@ -398,7 +397,6 @@ class TestSetupUv:
 
 class TestSetupWatchexec:
     @throttle_test(delta=HOUR)
-    @mark.only
     def test_main(
         self, *, token: Secret[str] | None, tmp_path: Path, capsys: CaptureFixture
     ) -> None:
