fa8eb5d546eda9c05b9ab4da1f0b94d19d2f1bdd
Derek Wan <d.wan@icloud.com>
2026-01-16 07:38:45 +0900

    Extract `sshd_config` as a standalone function (#61)
    Co-authored-by: github-actions-bot <noreply@github.com>

--- src/tests/configs/test_lib.py
+++ src/tests/configs/test_lib.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from utilities.text import strip_and_dedent
+
 from installer.configs.lib import (
     setup_authorized_keys,
     setup_ssh_config,
     setup_sshd_config,
+    sshd_config,
 )
 
 if TYPE_CHECKING:
@@ -25,3 +28,15 @@ class TestSetupSSHConfig:
 class TestSetupSSHDConfig:
     def test_main(self, *, tmp_path: Path) -> None:
         setup_sshd_config(root=tmp_path)
+
+
+class TestSSHDConfig:
+    def test_main(self) -> None:
+        result = sshd_config()
+        expected = strip_and_dedent("""
+            PasswordAuthentication no
+            PermitRootLogin no
+            PubkeyAcceptedAlgorithms ssh-ed25519
+            PubkeyAuthentication yes
+        """)
+        assert result == expected
